dcc9ea3dfc24bec7021b1ad1accf6a4fb51796df
Dionysis <dionysisterzios@gmail.com>
2022-10-15 18:35:47 +0300

    Add text when passwords don't match

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const password = document.querySelector('#password');
 const passwordConfirmation = document.querySelector('#password_confirmation');
+const passwordLabel = document.querySelector('label[for="password"]');
+const sheet = document.styleSheets[1];
+
+// The text under password input
+let exists = true;
 let passwordValue = null;
 let passwordConfirmationValue = null;
+
+// Insert text under password input
+sheet.insertRule('label[for="password"]::after { content: "* Passwords do not match"; color: red; font-size: 0.8rem }');
 
 function check(event) {
     if (event.target.id === 'password') {
@@ -11,11 +19,21 @@ function check(event) {
     }
 
     if (passwordValue === passwordConfirmationValue && passwordValue != '' && passwordConfirmationValue != '') {
-        password.classList.toggle('error');
-        passwordConfirmation.classList.toggle('error');
+        password.classList.remove('error');
+        passwordConfirmation.classList.remove('error');
     } else {
         password.classList.add('error');
         passwordConfirmation.classList.add('error');
+    }
+
+    if (password.classList.contains('error')) {
+        if (!exists) {
+            sheet.insertRule('label[for="password"]::after { content: "* Passwords do not match"; color: red; font-size: 0.8rem }');
+            exists = true;
+        }
+    } else {
+        sheet.deleteRule('[for="password"]::after');
+        exists = false;
     }
 }
 
